184bda7596a679f8e832203758a68552e7b5392f
PyMo <moritz.limbacher@gmail.com>
2025-04-02 10:59:28 +0200

    Auto-commit at 2025-04-02 10:59:28

--- SpectraStepAndGlue/StepNGlueSpec2.py
+++ SpectraStepAndGlue/StepNGlueSpec2.py
@@ -18,7 +18,7 @@ class SpectraGluingApp:
         self.open_button2 = tk.Button(master, text="Open Spectrum 2", command=self.open_spectrum2)
         self.open_button2.pack()
 
-        self.glue_button = tk.Button(master, text="Glue Spectra", command=self.glue_spectra)
+        self.glue_button = tk.Button(master, text="Glue Spectra", command=self.interpoverlap)
         self.glue_button.pack()
 
         self.save_button = tk.Button(master, text="Save Result", command=self.save_result)
@@ -66,7 +66,7 @@ class SpectraGluingApp:
 
     def glue_spectra(self):
         if self.spectrum1 is None or self.spectrum2 is None:
-            messagebox.showerror("Error", "Please load both spectra first.")
+            print("Please load both spectra first.")
             return
 
         # Extract wavelengths and values from both spectra
@@ -84,13 +84,13 @@ class SpectraGluingApp:
 
         self.dfs2 = pd.DataFrame({'WL': wavelengths2, 'counts': values2})
         # remove overlapping wavelengths from spectrum 2
+        print('removing overlapping wavelengths')
         self.dfs1, self.dfs2 = self.removeoverlap(self.dfs1, self.dfs2)
 
         # plot dfs1 and dfs2
         plt.plot(self.dfs1['WL'], self.dfs1['counts'], label='Spectrum 1')
         plt.plot(self.dfs2['WL'], self.dfs2['counts'], label='Spectrum 2')
         plt.show()
-
 
         # Find overlapping wavelengths
         common_wavelengths = np.intersect1d(wavelengths1, wavelengths2)
@@ -159,9 +159,6 @@ class SpectraGluingApp:
         # Remove overlapping wavelengths of the pd.dataframes S1 and S2
         # S1 is the first spectrum, S2 is the second spectrum
         # Find overlapping wavelengths
-        overlap = np.intersect1d(S1['WL'], S2['WL'])
-        if len(overlap) == 0:
-            return S1, S2
 
         # Get start and end of overlap
         wl1s = S1['WL'].iloc[0]
@@ -189,6 +186,81 @@ class SpectraGluingApp:
                 S2 = S2[S2['counts'] <= S2[S2['WL'] < overlapcenter]['counts'].max()]
                 S1 = S1[S1['WL'] > overlapcenter]     
                 S2 = S2[S2['counts'] <= S2[S2['WL'] > overlapcenter]['counts'].max()]
+        return S1, S2
+
+    def interpoverlap(self, S1, S2):
+        # Remove overlapping wavelengths of the pd.dataframes S1 and S2
+        # S1 is the first spectrum, S2 is the second spectrum
+        # Find overlapping wavelengths
+
+        # Get start and end of overlap
+        wl1s = S1['WL'].iloc[0]
+        wl1e = S1['WL'].iloc[-1]
+        wl2s = S2['WL'].iloc[0]
+        wl2e = S2['WL'].iloc[-1]
+        print('wl1s:', wl1s, 'wl1e:', wl1e, 'wl2s:', wl2s, 'wl2e:', wl2e)
+
+        # Determine and remove overlapping wavelengths
+        # 1st case: S1 is before S2
+        if wl1s < wl2e:
+            if wl1e > wl2s:
+                print('case 1')
+                # Find overlap region
+                overlap_start = max(wl1s, wl2s)
+                overlap_end = min(wl1e, wl2e)
+                
+                # Create interpolation points in overlap region
+                overlap_points = np.linspace(overlap_start, overlap_end, 100)
+                
+                # Interpolate both spectra in overlap region
+                S1_interp = np.interp(overlap_points, 
+                                    S1[S1['WL'].between(overlap_start, overlap_end)]['WL'],
+                                    S1[S1['WL'].between(overlap_start, overlap_end)]['counts'])
+                S2_interp = np.interp(overlap_points, 
+                                    S2[S2['WL'].between(overlap_start, overlap_end)]['WL'],
+                                    S2[S2['WL'].between(overlap_start, overlap_end)]['counts'])
+                
+                # Create weighted average transition
+                weights = np.linspace(1, 0, len(overlap_points))
+                interp_counts = S1_interp * weights + S2_interp * (1 - weights)
+                
+                # Create new dataframe with interpolated region
+                S1 = S1[S1['WL'] < overlap_start]
+                S2 = S2[S2['WL'] > overlap_end]
+                
+                # Add interpolated region to S1
+                overlap_df = pd.DataFrame({'WL': overlap_points, 'counts': interp_counts})
+                S1 = pd.concat([S1, overlap_df]).sort_values('WL')
+        # 2nd case: S1 is after S2
+        elif wl2s < wl1e:
+            if wl2e > wl1s:
+                print('case 2')
+                # Find overlap region
+                overlap_start = max(wl2s, wl1s)
+                overlap_end = min(wl2e, wl1e)
+                
+                # Create interpolation points in overlap region
+                overlap_points = np.linspace(overlap_start, overlap_end, 100)
+                
+                # Interpolate both spectra in overlap region
+                S2_interp = np.interp(overlap_points,
+                                    S2[S2['WL'].between(overlap_start, overlap_end)]['WL'],
+                                    S2[S2['WL'].between(overlap_start, overlap_end)]['counts'])
+                S1_interp = np.interp(overlap_points,
+                                    S1[S1['WL'].between(overlap_start, overlap_end)]['WL'],
+                                    S1[S1['WL'].between(overlap_start, overlap_end)]['counts'])
+                
+                # Create weighted average transition
+                weights = np.linspace(1, 0, len(overlap_points))
+                interp_counts = S2_interp * weights + S1_interp * (1 - weights)
+                
+                # Create new dataframe with interpolated region
+                S2 = S2[S2['WL'] < overlap_start]
+                S1 = S1[S1['WL'] > overlap_end]
+                
+                # Add interpolated region to S2
+                overlap_df = pd.DataFrame({'WL': overlap_points, 'counts': interp_counts})
+                S2 = pd.concat([S2, overlap_df]).sort_values('WL')
         return S1, S2
 
 if __name__ == "__main__":
